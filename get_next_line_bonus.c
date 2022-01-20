27bfb626f7b2403d65409ed635753f7816c9a3bc
Norman LE BRETON <nle-bret@e2r4p1.clusters.42paris.fr>
2022-01-20 10:58:17 +0100

    modidy the location of the true  header

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 ssize_t	fill_buffer(int fd, char *buffer)
 {
